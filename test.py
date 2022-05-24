70f41cd31670d33a40ba27091e237e613705cafa
Fuu <98339035+notyourfriends@users.noreply.github.com>
2022-05-24 13:30:12 +0700

    testing

--- test.py
+++ test.py
@@ -1,1 +1,10 @@
-print('hello world')
+from kivymd.app import MDApp
+from kivy.lang import Builder
+
+class MainApp(MDApp):
+	def build(self):
+		self.theme_cls.theme_style = 'Dark'
+		self.theme_cls.primary_palette = 'Blue'
+		return Builder.load_file('test.kv')
+
+MainApp().run()
