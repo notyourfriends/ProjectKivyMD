9bef3b0a22dcde1be1fa52ed7e632b564dc68c52
Fuu <98339035+notyourfriends@users.noreply.github.com>
2022-05-28 21:13:33 +0700

    Create table.py

---
+++ table.py
@@ -0,0 +1,48 @@
+from kivy.lang import Builder
+from kivymd.app import MDApp
+from kivymd.uix.screen import Screen
+from kivymd.uix.datatables import MDDataTable
+from kivy.metrics import dp #Display Pixles
+
+class MainApp(MDApp):
+	def build(self):
+		#Define Screen
+		screen = Screen()
+		#Define table
+		table = MDDataTable(
+			pos_hint = {'center_x': .5, 'center_y': .5},
+			size_hint = (0.9, 0.6),
+			check = True,
+
+			column_data = [
+				('First Name', dp(30)),
+				('Last Name', dp(30)),
+				('Email Address', dp(50)),
+				('Phone Number', dp(30)),
+			],
+			row_data = [
+				('Muhamad Refo', 'Alpha Rizky', 'refo.alpha@gmail.com', '081255667780'),
+				('Fuu', 'Nii', 'fuunii99@gmail.com', '085719328218'),
+			]
+
+			)
+		#Bind Table
+		table.bind(on_check_press = self.checked)
+		table.bind(on_row_press = self.row_checked)
+
+
+
+		self.theme_cls.theme_style = 'Light'
+		self.theme_cls.primary_palette = 'BlueGray'
+		#return Builder.load_file('table.kv')
+
+		#add Table widget to screen
+		screen.add_widget(table)
+		return screen
+
+	def checked(self, instance_table, current_row):
+		print(instance_table, current_row)
+	def row_checked(self,instance_table, instance_row):
+		print(instance_table, instance_row)
+
+MainApp().run()
