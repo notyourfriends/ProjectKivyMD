e914bd54999c672dc534c3fc02422aa6fe07cffe
Fuu <98339035+notyourfriends@users.noreply.github.com>
2022-06-08 18:56:10 +0700

    Update Logic

--- tictactoe.py
+++ tictactoe.py
@@ -10,6 +10,74 @@ class MainApp(MDApp):
     #Define who's turn it is
     turn = "X"
 
+    #Keep track of win or lose
+    winner = False
+
+    #End The Game
+    def end_game(self, a,b,c):
+        self.winner =True
+        a.color = 'red'
+        b.color = 'red'
+        c.color = 'red'
+
+        #Disable the buttons
+        self.disable_all_buttons()
+
+        #Set Label for winner
+        self.root.ids.score.text = f"{a.text} Wins"
+
+    def disable_all_buttons(self):
+        #Disable the Buttons
+        self.root.ids.btn1.disabled = True
+        self.root.ids.btn2.disabled = True
+        self.root.ids.btn3.disabled = True
+        self.root.ids.btn4.disabled = True
+        self.root.ids.btn5.disabled = True
+        self.root.ids.btn6.disabled = True
+        self.root.ids.btn7.disabled = True
+        self.root.ids.btn8.disabled = True
+        self.root.ids.btn9.disabled = True
+
+    def win(self):
+        #Across
+        if self.root.ids.btn1.text != "" and self.root.ids.btn1.text == self.root.ids.btn2.text == self.root.ids.btn3.text:
+            self.end_game(self.root.ids.btn1, self.root.ids.btn2, self.root.ids.btn3)
+        if self.root.ids.btn4.text != "" and self.root.ids.btn4.text == self.root.ids.btn5.text == self.root.ids.btn6.text:
+            self.end_game(self.root.ids.btn4, self.root.ids.btn5, self.root.ids.btn6)
+        if self.root.ids.btn7.text != "" and self.root.ids.btn7.text == self.root.ids.btn8.text == self.root.ids.btn9.text:
+            self.end_game(self.root.ids.btn7, self.root.ids.btn8, self.root.ids.btn9)
+        
+        
+        #Down
+        if self.root.ids.btn1.text != "" and self.root.ids.btn1.text == self.root.ids.btn4.text == self.root.ids.btn7.text:
+            self.end_game(self.root.ids.btn1, self.root.ids.btn4, self.root.ids.btn7)
+        if self.root.ids.btn2.text != "" and self.root.ids.btn2.text == self.root.ids.btn5.text == self.root.ids.btn8.text:
+            self.end_game(self.root.ids.btn2, self.root.ids.btn5, self.root.ids.btn8)
+        if self.root.ids.btn3.text != "" and self.root.ids.btn3.text == self.root.ids.btn6.text == self.root.ids.btn9.text:
+            self.end_game(self.root.ids.btn3, self.root.ids.btn6, self.root.ids.btn9)
+
+        #Diagonal
+        if self.root.ids.btn1.text != "" and self.root.ids.btn1.text == self.root.ids.btn5.text == self.root.ids.btn9.text:
+            self.end_game(self.root.ids.btn1, self.root.ids.btn5, self.root.ids.btn9)
+        if self.root.ids.btn3.text != "" and self.root.ids.btn3.text == self.root.ids.btn5.text == self.root.ids.btn7.text:
+            self.end_game(self.root.ids.btn3, self.root.ids.btn5, self.root.ids.btn7)
+        
+        self.no_winner()
+    
+    def no_winner(self):
+        if self.winner == False and \
+        self.root.ids.btn1.disabled == True and \
+        self.root.ids.btn2.disabled == True and \
+        self.root.ids.btn3.disabled == True and \
+        self.root.ids.btn4.disabled == True and \
+        self.root.ids.btn5.disabled == True and \
+        self.root.ids.btn6.disabled == True and \
+        self.root.ids.btn7.disabled == True and \
+        self.root.ids.btn8.disabled == True and \
+        self.root.ids.btn9.disabled == True:
+            self.root.ids.score.text = "It's a Tie"
+
+
     def presser(self, btn):
         if self.turn == "X":
             btn.text = "X"
@@ -21,10 +89,30 @@ class MainApp(MDApp):
             btn.disabled = True
             self.root.ids.score.text = "X's Turn!"
             self.turn = "X"
+
+        #Check to see if someone is win
+        self.win()
     
     def restart(self):
         #Reset Who's Turn it is
         self.turn = "X"
+        
+        #Reset the Score label
+        self.root.ids.score.text = "X Goes First"
+
+        #Reset the winner variable
+        self.winner = False
+
+        #Reset the color
+        self.root.ids.btn1.color = "green"
+        self.root.ids.btn2.color = "green"
+        self.root.ids.btn3.color = "green"
+        self.root.ids.btn4.color = "green"
+        self.root.ids.btn5.color = "green"
+        self.root.ids.btn6.color = "green"
+        self.root.ids.btn7.color = "green"
+        self.root.ids.btn8.color = "green"
+        self.root.ids.btn9.color = "green"
 
         # Enable the buttons
         self.root.ids.btn1.disabled = False
